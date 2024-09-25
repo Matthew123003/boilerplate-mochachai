8c34d4c0a5b2dbdfddc150c5801f6b8291e1865c
matthew123003 <matthew123003@gmail.com>
2024-09-25 17:25:32 -0400

    First test done

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,10 +1,12 @@
+// npm test
+// COMMAND TO RUN TESTS
 const chai = require('chai');
 const assert = chai.assert;
 
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
-    test('#isNull, #isNotNull', function () {
+    test('.isNull, #isNotNull', function () {
       assert.fail(null, 'This is an optional error description - e.g. null is null');
       assert.fail(1, '1 is not null');
     });
